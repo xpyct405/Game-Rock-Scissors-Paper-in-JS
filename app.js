cf30ab0ce8bda50deb2cab75e686917cf1b6ceff
Rustam Kuzyaev <xpyct405@yandex.ru>
2023-03-19 23:38:24 +0300

    feat: add animation, change styles, add fonts, add code JS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let optional = document.querySelectorAll('.user-optional')
 let userResult = document.querySelector('.user-result_item')
 let gptResult = document.querySelector('.gpt-result_item')
 let result = document.querySelector('.result-text')
+let gameContainer = document.querySelector('.container')
 
 // Проходимся по всей выборке
 for (let i = 0; i < optional.length; i++) {
@@ -13,46 +14,61 @@ for (let i = 0; i < optional.length; i++) {
         // Добавляем класс active элементу по которому мы кликаем
         optional[i].classList.add('active')
 
-        // Меняем userResult при клике по любой картинке 
-        userResult.src = optional[i].src;
+        // В самом начале в каждой итерации заменяются значки компьютера и юзера на rock.png
+        userResult.src = gptResult.src = './img/rock.png'
+
+        result.textContent = `Wait for it`
+
+        // При клике по любому элементу сработает начало анимации (тряска кулаками)
+        gameContainer.classList.add('start-animation')
+
+        // Задаём таймер измеений что внутри. Произодёт через 2500ms
+        let timeOut = setTimeout(()=> {
+       
+            // по истечению 2500ms анимация удалится.
+            gameContainer.classList.remove('start-animation')
+
+            // Меняем userResult при клике по любой картинке 
+            userResult.src = optional[i].src;
         
-        // Получаем рандомное число от 0 до 3
-        let randomNumber = Math.floor(Math.random() * 3);
+            // Получаем рандомное число от 0 до 3
+            let randomNumber = Math.floor(Math.random() * 3);
         
-        // Создаём ряд возможных значений GptResult
-        let gptImages = ['./img/rock.png','./img/scissors.png','./img/paper.png'];
+            // Создаём ряд возможных значений GptResult
+            let gptImages = ['./img/rock.png','./img/scissors.png','./img/paper.png'];
 
-        // Меняем gptResult на рандомную картинку из ряда выше
-        gptResult.src = gptImages[randomNumber]
+            // Меняем gptResult на рандомную картинку из ряда выше
+            gptResult.src = gptImages[randomNumber]
 
-        // Каждому элменту задаём значение в буквах
-        let userValue = ['R', 'S', 'P'][i]
+            // Каждому элменту задаём значение в буквах
+            let userValue = ['R', 'S', 'P'][i]
 
-        // Каждому элменту задаём значение в буквах
-        let gptValue = ['R', 'S', 'P'][randomNumber]
+            // Каждому элменту задаём значение в буквах
+            let gptValue = ['R', 'S', 'P'][randomNumber]
 
-        console.log(userValue, gptValue);
+            console.log(userValue, gptValue);
 
-        // Создаём объект в который пропишем всевозможные варианты событий
-        let outComes = {
-            RR: 'Ничья',
-            RS: 'Пользователь',
-            RP: 'GPT',
+            // Создаём объект в который пропишем всевозможные варианты событий
+            let outComes = {
+                RR: 'Ничья',
+                RS: 'Пользователь',
+                RP: 'GPT',
 
-            SR: 'GPT',
-            SS: 'Ничья',
-            SP: 'Пользователь',
+                SR: 'GPT',
+                SS: 'Ничья',
+                SP: 'Пользователь',
 
-            PR: 'Пользователь',
-            PS: 'GPT',
-            PP: 'Ничья',
-        }
+                PR: 'Пользователь',
+                PS: 'GPT',
+                PP: 'Ничья',
+            }
         
-        // Записываем значения и сопостовляем с outComes
-        let outComeValue  = outComes[userValue + gptValue];
-        console.log(outComeValue);
+            // Записываем значения и сопостовляем с outComes
+            let outComeValue  = outComes[userValue + gptValue];
+            console.log(outComeValue);
 
-        console.log(result.innerHTML);
-        result.innerHTML = outComeValue
+            console.log(result.innerHTML);
+            result.innerHTML = userValue === gptValue ? "Match Draw!" : `${outComeValue} Won!`
+            }, 3000)
     })
 }
